90fad2d0f040c15be840c3125f8b0a0f7f5281aa
bxcar <malanchukdima@mail.ru>
2018-08-06 12:46:12 +0300

    several changes

---
+++ wp-content/themes/main/templates/funktsii-crm.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ * Template Name: funktsii-crm
+ */
+
+get_header();
+?>
+    <div class="block-content">
+        <div class="features-content">
+            <div class="rasdel-title"><div class="container"><div class="back"><a href="#"><span class="ico"><img src="../i/ico-back.png"></span> <span class="text">Назад</span></a></div><h1 class="margin-0">Функции CRM систем</h1></div></div>
+            <div class="wrap1">
+                <div class="wrap1-left"></div>
+                <div class="wrap1-center container">
+                    <div class="items">
+                        <div class="row">
+                            <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                <div class="box box1">
+                                    <div class="number">01</div>
+                                    <div class="text">Мы подберем для Вас нужную платформу и присоединим к ней все необходимые Вашему бизнесу инструменты.</div>
+                                </div>
+                            </div>
+                            <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                <div class="box box2">
+                                    <div class="number">02</div>
+                                    <div class="text">Каждое обращение Клиента будет зарегистрировано автоматически и попадет в Вашу цифровую воронку продаж!</div>
+                                </div>
+                            </div>
+                            <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                <div class="box box3">
+                                    <div class="number">03</div>
+                                    <div class="text">Потенциальная сделка, будет двигаться по этапам воронки и подсказывать менеджерам о необходимости совершения следующих шагов.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="wrap1-right"><div></div></div>
+            </div>
+
+
+            <div class="wrap2">
+                <div class="wrap2-left"></div>
+                <div class="wrap2-center container">
+                    <div class="head">
+                        Мы создаем нашим клиентам единую технологичную<br />среду, которая автоматически:
+                    </div>
+                    <div class="cont">
+
+                        <div class="vert"></div>
+
+                        <div class="box box-left box1">
+                            <div class="line"></div><div class="circle"></div>
+                            <div class="number">
+                                01
+                                <div class="ico"><img src="../i/features-ico-ribbon.png"></div>
+                            </div>
+                            <div class="text">Принимает и обрабатывает заявки клиентов, собирая данные с ваших </div>
+                            <div class="punkts">
+                                <div class="">
+                                    <span class="col-xs-12 col-sm-3 col-md-3 col-lg-3">Сайтов</span>
+                                    <span class="col-xs-12 col-sm-3 col-md-3 col-lg-3">Почты</span>
+                                    <span class="col-xs-12 col-sm-6 col-md-6 col-lg-6">Мессенджеров</span>
+                                    <span class="col-xs-12 col-sm-6 col-md-6 col-lg-6">Аккаунтах в соц. сетях</span>
+                                    <span class="col-xs-12 col-sm-6 col-md-6 col-lg-6">С телефонов (даже мобильных)</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="box box-right box2">
+                            <div class="line"></div><div class="circle"></div>
+                            <div class="number">
+                                02
+                                <div class="ico"><img src="../i/features-ico-bullhorn.png"></div>
+                            </div>
+                            <div class="text">Подсказывает менеджерам как построить беседу с помощью всплывающих скриптов продаж</div>
+                        </div>
+
+                        <div class="box box-left box3">
+                            <div class="line"></div><div class="circle"></div>
+                            <div class="number">
+                                03
+                                <div class="ico"><img src="../i/features-ico-new_post.png"></div>
+                            </div>
+                            <div class="text">Ставит задачи соответствующим подразделениям, будь то складской работник или замерщик</div>
+                        </div>
+
+                        <div class="box box-right box4">
+                            <div class="line"></div><div class="circle"></div>
+                            <div class="number">
+                                04
+                                <div class="ico"><img src="../i/features-ico-bell.png"></div>
+                            </div>
+                            <div class="text">Оповещает клиентов и сотрудников о прохождении ключевых стадий при помощи:</div>
+                            <div class="punkts">
+                                <div class="">
+                                    <span class="col-xs-12 col-sm-4 col-md-4 col-lg-4">E-mail</span>
+                                    <span class="col-xs-12 col-sm-3 col-md-3 col-lg-3">SMS</span>
+                                    <span class="col-xs-12 col-sm-12 col-md-12 col-lg-12">push-уведомлений мессенджеров</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="box box-left box5">
+                            <div class="line"></div><div class="circle"></div>
+                            <div class="number">
+                                05
+                                <div class="ico"><img src="../i/features-ico-document.png"></div>
+                            </div>
+                            <div class="text">Генерирует необходимые документы и при необходимости запускает процесс их согласования</div>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="wrap2-right"></div>
+            </div>
+
+
+            <div class="wrap3">
+                <div class="container">
+                    <div class="head">При помощи специальных<br />дополнительных настроек Вы сможете:</div>
+                    <div class="head-sub"></div>
+                    <div class="cont">
+                        <div class="row">
+                            <div class="items">
+                                <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                    <div class="box">
+                                        <div class="ico"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+                                        <div class="text">Оповестить клиентов о новом товаре с помощью красочных именных E-mail рассылок</div>
+                                    </div>
+                                </div>
+                                <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                    <div class="box">
+                                        <div class="ico"><i class="fa fa-gift" aria-hidden="true"></i></div>
+                                        <div class="text">Поздравлять клиентов с Днем Рождения, опубликовав его на их страничке в социальной сети</div>
+                                    </div>
+                                </div>
+                                <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                    <div class="box">
+                                        <div class="ico"><i class="fa fa-folder-o" aria-hidden="true"></i></div>
+                                        <div class="text">В 2 клика мыши формировать счета, договора и другие документы, благодаря настроенному электронному документообороту</div>
+                                    </div>
+                                </div>
+                                <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                    <div class="box">
+                                        <div class="ico"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                                        <div class="text">В 2 клика мыши позвонить клиенту</div>
+                                    </div>
+                                </div>
+                                <div class="item col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                                    <div class="box">
+                                        <div class="ico"><i class="fa fa-bar-chart" aria-hidden="true"></i></div>
+                                        <div class="text">В 2 нажатия пальцем на экран телефона или планшета увидеть всю динамику продаж, включая прогноз будущих сделок</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix"></div>
+    </div><!-- /.content -->
+<?php get_footer(); ?>
---
+++ wp-content/themes/main/templates/services.php
@@ -0,0 +1,272 @@
+<?php
+/**
+ * Template Name: services
+ */
+
+get_header();
+?>
+    <div class="block-content">
+        <div class="uslugi-content">
+            <div class="rasdel-title">
+                <div class="container">
+                    <div class="back"><a href="#"><span class="ico">
+          <img src="../i/ico-back.png">
+          </span> <span class="text">Назад</span></a></div>
+                    <h1 class="margin-0">Отраслевые CRM-системы</h1></div>
+            </div>
+            <div class="items"><pre style="display:none">100</pre>
+                <div class="container">
+                    <div class="row">         <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box10">
+                                <div class="text">CRM для недвижимости</div>
+                                <div class="more"><a href="crm-dlya-agentstva-nedvizhimosti/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box14">
+                                <div class="text">CRM для автосервиса</div>
+                                <div class="more"><a href="crm-dlya-avtoservisa/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box13">
+                                <div class="text">CRM для интернет-магазина</div>
+                                <div class="more"><a href="crm-dlya-internet-magazina/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box12">
+                                <div class="text">CRM для малого бизнеса</div>
+                                <div class="more"><a href="crm-dlya-malogo-biznesa/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box16">
+                                <div class="text">CRM для оптовой торговли</div>
+                                <div class="more"><a href="crm-dlya-optovoj-torgovli/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box8">
+                                <div class="text">CRM для отдела продаж</div>
+                                <div class="more"><a href="crm-dlya-otdela-prodazh/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box11">
+                                <div class="text">CRM для салона красоты</div>
+                                <div class="more"><a href="crm-dlya-salona-krasoty/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box7">
+                                <div class="text">CRM для рекламного агентства</div>
+                                <div class="more"><a href="crm-dlya-reklamnogo-agentstva/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box9">
+                                <div class="text">CRM для стоматологии</div>
+                                <div class="more"><a href="crm-dlya-stomatologii/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box6">
+                                <div class="text">CRM для производства</div>
+                                <div class="more"><a href="crm-dlya-proizvodstva/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box20">
+                                <div class="text">CRM для клиник</div>
+                                <div class="more"><a href="crm-dlya-klinik/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box21">
+                                <div class="text">CRM для кофейни</div>
+                                <div class="more"><a href="crm-dlya-kofejni/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box22">
+                                <div class="text">CRM для лендинга</div>
+                                <div class="more"><a href="crm-dlya-lendinga/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box23">
+                                <div class="text">CRM для транспортных компаний</div>
+                                <div class="more"><a href="crm-dlya-logistiki/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box19">
+                                <div class="text">CRM для фитнес клуба</div>
+                                <div class="more"><a href="crm-dlya-fitnesa/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box24">
+                                <div class="text">CRM для розничного магазина</div>
+                                <div class="more"><a href="crm-dlya-roznichnogo-magazina/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box25">
+                                <div class="text">CRM для школы танцев</div>
+                                <div class="more"><a href="crm-dlya-shkoly-tancev/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box26">
+                                <div class="text">CRM для склада</div>
+                                <div class="more"><a href="crm-dlya-sklada/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box18">
+                                <div class="text">CRM для call-центра</div>
+                                <div class="more"><a href="crm-dlya-call-tsentra/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box27">
+                                <div class="text">CRM для строительных компаний</div>
+                                <div class="more"><a href="crm-dlya-stroitelnykh-kompanij/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box28">
+                                <div class="text">CRM для торговли</div>
+                                <div class="more"><a href="crm-dlya-torgovly/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box29">
+                                <div class="text">CRM для турагентства</div>
+                                <div class="more"><a href="crm-dlya-turagentstva/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box30">
+                                <div class="text">CRM для языковых школ</div>
+                                <div class="more"><a href="crm-dlya-yazykovoj-shkoly/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box31">
+                                <div class="text">CRM для юридической компании</div>
+                                <div class="more"><a href="crm-dlya-yuridicheskoy-kompanii/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                        <div class="item col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                            <div class="box box17">
+                                <div class="text">CRM для банков</div>
+                                <div class="more"><a href="crm-dlya-bankov/index.html">Подробнее</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="wrap1 experts">
+                <div class="wrap1-left">
+                    <div class=""></div>
+                </div>
+                <div class="wrap1-center container">
+                    <div class="row">
+                        <div class="item item1 col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                            <div class="box">
+                                <div class="head">Мы эксперты</div>
+                                <div class="cont">
+                                    <div class="row">                 <ul class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
+                                            <li>АВТОМАТИЗАЦИИ ПРОДАЖ</li>
+                                            <li>АВТОМАТИЗАЦИИ СОПРОВОЖДЕНИЯ/СЕРВИСА</li>
+                                        </ul>
+                                        <ul class="col-xs-12 col-sm-7 col-md-7 col-lg-7">
+                                            <li>АВТОМАТИЗАЦИИ МАРКЕТИНГА</li>
+                                            <li>АВТОМАТИЗАЦИИ ПОДДЕРЖКИ ПРОДАЖ</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="item item2 col-xs-12 col-sm-4 col-md-4 col-lg-4"></div>
+                    </div>
+                </div>
+                <div class="wrap1-right">
+                    <div class=""></div>
+                </div>
+            </div>
+            <div class="wrap2">
+                <div class="wrap2-left"></div>
+                <div class="wrap2-center container">
+                    <div class="head">
+                        <div class="hidden-xs">Мы подбираем инструменты <span>на самом деле</span><br>
+                            нужные нашим клиентам</div>
+                        <div class="visible-xs-block">Мы подбираем инструменты<br />
+                            <span>на самом деле</span><br>
+                            нужные нашим клиентам</div>
+                    </div>
+                    <div class="cont">
+                        <ul class="row">
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Лучшие CRM системы</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Email рассылки</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Онлайн чаты</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Сквозная бизнес-аналитика</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Сегментация клиентов</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Обратный звонок</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Call tracking</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Документооборот</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Работа с соц.сетями</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Электронные Скрипты продаж</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Ведение проектов</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Онлайн бронирование</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">SMS рассылки</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="wrap2-right"></div>
+            </div>
+            <div class="wrap3">
+                <div class="wrap3-left"></div>
+                <div class="wrap3-center container">
+                    <div class="head">Идеальная телефония существенная составляющая идеальной автоматизации, доверьте этот вопрос нам:</div>
+                    <div class="cont">
+                        <ul>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Облачная АТС с настройкой под бизнес-логику Заказчика</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Междугородняя и Международная связь</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Организация call-центра</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Телефонные номера</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Cлужебная мобильная связь</li>
+                            <li class="col-xs-12 col-sm-4 col-md-4 col-lg-4">Телекоммуникационная аппаратура</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="wrap3-right"></div>
+            </div>
+            <div class="wrap2">
+                <div class="wrap2-left"></div>
+                <div class="wrap2-center container">
+                    <div class="cont">
+                        <p>В представлении неподготовленных людей CRM-системы находят применение только в интернет-магазинах, но на практике они могут использоваться и в иных направлениях бизнеса. Отраслевые CRM системы горизонтального характера помогают в решении вопросов, направленных на развитие бизнеса, связанных с аналитикой и контролем работы сотрудников. Если говорить о сфере применения отраслевых CRM-решений, то это любая отрасль бизнеса, в которой обязательно присутствует непосредственное общение менеджера с клиентом.</p>
+                        <p>Каталог отраслевых направлений для CRM-систем amoCRM и Битрикс24 постоянно расширяется, так как целесообразность продукта проверена на практике разнообразными предприятиями торговли и предоставления услуг. Мы поможем подобрать удобный вариант конфигурации для предприятия любого уровня, в том числе:</p>
+                        <ul>
+                            <li>агентствам недвижимости;</li>
+                            <li>медицинским учреждениям;</li>
+                            <li>автосервисам и ремонтным предприятиям;</li>
+                            <li>банкам и финансовым компаниям;</li>
+                            <li>интернет-магазинам (выгодно показывает себя в магазинах с широким ассортиментом);</li>
+                            <li>транспортно-логистическим компаниям.</li>
+                        </ul>
+                        <p>Причем в каждом конкретном случае мы подбираем CRM, разрабатываем разнообразные дополнения и надстройки, помогающие вести успешный бизнес.</p>
+                    </div>
+                </div>
+                <div class="wrap2-right"></div>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+    <div class="clearfix"></div>
+    </div><!-- /.content -->
+<?php get_footer(); ?>
